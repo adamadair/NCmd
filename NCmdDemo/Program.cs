8f7f9a4c9b7e5cc67188df134da69109ead319a5
adamadair@outlook.com <adamadair@outlook.com>
2016-07-16 18:24:17 -0500

    Added ArgumentParser to Demo

--- NCmdDemo/Program.cs
+++ NCmdDemo/Program.cs
@@ -1,11 +1,33 @@
-﻿namespace NCmdDemo
+﻿using System;
+using NCmd;
+
+namespace NCmdDemo
 {
     internal class Program
     {
+        // setup the command line argument parser.
+        static ArgumentParser ArgParse = new ArgumentParser () {
+            { "h|?|help", "Display usage message and exit.", v => ShowHelp () },
+            { "v|version", "Display version information.", v=>ShowVersion() }
+        };
+
         static void Main(string[] args)
         {
+            ArgParse.Parse (args);
             var ds = new DemoShell();
             ds.CmdLoop();
+        }
+
+        private static void ShowHelp ()
+        {
+            ArgParse.WriteArgumentDescriptions (Console.Out);
+            Environment.Exit (0);
+        }
+
+        private static void ShowVersion ()
+        {
+            Console.WriteLine ("NCmdDemo v 1.0");
+            Environment.Exit (0);
         }
     }
 }
